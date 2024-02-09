f288ab0d292fddfdb822975a7de126eeb1b509f0
juan <teamzzesp@gmail.com>
2024-02-09 12:08:11 +0100

    Funcionalidad tlf2 ok

--- src/Vehiculo.java
+++ src/Vehiculo.java
@@ -1,6 +1,11 @@
 
 public class Vehiculo {
-	public static void main(String[] args) {
+
+	public Vehiculo() {
 		System.out.println("Brrrrr… acaba de arrancar");
+	}
+
+	public static void main(String[] args) {
+		Vehiculo miMoto = new Vehiculo();
 	}
 }
